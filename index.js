3c19b6ae97e5c5e6e889ca8f4819027c970f0951
Su nombre <uiehcskuf@gmail.com>
2025-08-29 13:32:36 -0500

    se agregaron validaciones y mejoras del codigo

--- index.js
+++ index.js
@@ -9,3 +9,13 @@ function calculateTip(billAmount, tipPercentage) {
     const tip = (billAmount * tipPercentage) / 100;
     return tip;
 }
+function mostrarResultado() {
+    console.log("=== CALCULADORA DE PROPINA ===");
+    console.log("Ejemplo 1 - Input: calculateTip(100, 10)");
+    console.log("Ejemplo 1 - Output:", calculateTip(100, 10));
+    console.log("---");
+    console.log("Ejemplo 2 - Input: calculateTip(1524.33, 25)");
+    console.log("Ejemplo 2 - Output:", calculateTip(1524.33, 25));
+    console.log("---");
+}
+mostrarResultado();
