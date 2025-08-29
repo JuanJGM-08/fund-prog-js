2c2cb170e5b40646810c7e62303fde76b007611b
Su nombre <uiehcskuf@gmail.com>
2025-08-29 15:08:49 -0500

    Finalizaccion de codigo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function calculateTip(billAmount, tipPercentage) {
-    if (typeof billAmount !== 'number' || typeof tipPercentage !== 'nunmber'){
-        throw new Error ('Ambos parametros deben ser numeros');
+    if (typeof billAmount !== 'number' || typeof tipPercentage !== 'number') {
+        throw new Error('Ambos parámetros deben ser números');
     }
     if (billAmount < 0 || tipPercentage < 0) {
         throw new Error('Los valores deben ser positivos');
@@ -9,7 +9,8 @@ function calculateTip(billAmount, tipPercentage) {
     const tip = (billAmount * tipPercentage) / 100;
     return tip;
 }
-function mostrarResultado() {
+
+function mostrarResultados() {
     console.log("=== CALCULADORA DE PROPINA ===");
     console.log("Ejemplo 1 - Input: calculateTip(100, 10)");
     console.log("Ejemplo 1 - Output:", calculateTip(100, 10));
@@ -18,4 +19,5 @@ function mostrarResultado() {
     console.log("Ejemplo 2 - Output:", calculateTip(1524.33, 25));
     console.log("---");
 }
-mostrarResultado();
+
+mostrarResultados();
