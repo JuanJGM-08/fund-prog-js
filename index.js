24225cbe711678e35272a0baa9a353be035e818d
Su nombre <uiehcskuf@gmail.com>
2025-08-29 13:27:57 -0500

    se implemento la logica de propinas y calculo al codigo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function calculateTip(billAmount, tipPercentage) {}
+function calculateTip(billAmount, tipPercentage) {
+    if (typeof billAmount !== 'number' || typeof tipPercentage !== 'nunmber'){
+        throw new Error ('Ambos parametros deben ser numeros');
+    }
+    if (billAmount < 0 || tipPercentage < 0) {
+        throw new Error('Los valores deben ser positivos');
+    }
+
+    const tip = (billAmount * tipPercentage) / 100;
+    return tip;
+}
